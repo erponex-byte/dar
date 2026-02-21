ed084b38e65bcb347676b6ec0cd920f4688ef284
Your Name <you@example.com>
2026-02-21 05:06:05 +0200

    multi sales person 1

--- ica_web_responsive/__manifest__.py
+++ ica_web_responsive/__manifest__.py
@@ -2,7 +2,7 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'ICA Web Responsive',
+    'name': 'ICA Web Responsive 123',
     'author':"Agga, IdeaCode Academy",
     'version': '18.0.1.0',
     'depends': ['web', 'base_setup'],
